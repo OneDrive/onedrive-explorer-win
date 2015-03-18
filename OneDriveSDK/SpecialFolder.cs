bdaf4603143a453fdc9731b8eac49ec781a02259
Ryan Gregg <rgregg@microsoft.com>
2015-03-18 11:41:28 -0700

    Adding constants for special folders

---
+++ OneDriveSDK/SpecialFolder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneDrive
+{
+    public static class SpecialFolder
+    {
+        public static string Documents = "documents";
+        public static string Photos = "photos";
+        public static string CameraRoll = "cameraroll";
+        public static string Public = "public";
+    }
+}

485b2a7975937ff48ad3fd1ca8bc4ac27efb3413
Ryan Gregg <rgregg@users.noreply.github.com>
2015-05-18 10:58:13 -0700

    Add select to ViewChangesOptions

--- OneDriveSDK/CommandOptions/ViewChangesOptions.cs
+++ OneDriveSDK/CommandOptions/ViewChangesOptions.cs
@@ -18,6 +18,15 @@ namespace OneDrive
             set { SetValueForQueryString(ApiConstants.PageSizeQueryParameterKey, value.ToString()); }
         }
 
+        /// <summary>
+        /// Select properties on the expanded objects
+        /// </summary>
+        public string Select 
+        {
+            get { return ValueForQueryString(ApiConstants.SelectQueryParameterKey); }
+            set { SetValueForQueryString(ApiConstants.SelectQueryParameterKey, value); }
+        }
+
         public static ViewChangesOptions Default
         {
             get { return new ViewChangesOptions(); }
